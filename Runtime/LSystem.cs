302711b638a2cfe2a3757dff5c7d85c339f5db09
David Hofer <david@dna-studios.ch>
2022-12-21 13:37:18 +0100

    L-system computation

--- Runtime/LSystem.cs
+++ Runtime/LSystem.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Text;
+
 namespace UnityLib.LSystem {
 
     public class LSystem {
@@ -8,13 +11,32 @@ namespace UnityLib.LSystem {
         /// <param name="axiom"></param>
         /// <param name="rules"></param>
         /// <param name="n"></param>
-        public static string Compute(string axiom, string[] variables, string[] constants, Rule[] rules, int n) {
-            return "Hello";
+        public static string Compute(string axiom, IDictionary<string, string> rules, int n) {
+            string result = axiom;
+            for (int i = 0; i < n; i++) {
+                result = ApplyRules(result, rules);
+            }
+            return result;
         }
-    }
 
-    public struct Rule {
-        public string predecessor;
-        public string successor;
+        /// <summary>
+        /// Applies the rules against the given string.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="alphabet"></param>
+        /// <param name="rules"></param>
+        /// <returns></returns>
+        private static string ApplyRules(string s, IDictionary<string, string> rules) {
+            StringBuilder builder = new StringBuilder();
+            foreach (char c in s) {
+                string predecessor = c.ToString();
+                if (rules.ContainsKey(predecessor)) {
+                    builder.Append(rules[predecessor]);
+                } else {
+                    builder.Append(predecessor);
+                }
+            }
+            return builder.ToString();
+        }
     }
 }
--- Tests/LSystemTests.cs
+++ Tests/LSystemTests.cs
@@ -1,11 +1,77 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 using UnityLib.LSystem;
 
 public class LSystemTests {
 
+    /// <summary>
+    /// Taken from https://en.wikipedia.org/wiki/L-system
+    /// 
+    /// Variables: A, B
+    /// Axiom: A
+    /// Rules: (A => AB), (B => A)
+    /// </summary>
     [Test]
-    public void LSystemTestsSimplePasses() {
-        string result = LSystem.Compute("A", new string[] { "A", "B" }, new string[] { "+" }, new Rule[] { }, 1);
-        Assert.AreEqual(result, "Hello");
+    public void ItShouldApplyRules() {
+        string axiom = "A";
+
+        IDictionary<string, string> rules = new Dictionary<string, string>() {
+            { "A", "AB" },
+            { "B", "A" }
+        };
+
+        string result = LSystem.Compute(axiom, rules, 0);
+        Assert.AreEqual(result, "A");
+
+        result = LSystem.Compute(axiom, rules, 1);
+        Assert.AreEqual(result, "AB");
+
+        result = LSystem.Compute(axiom, rules, 2);
+        Assert.AreEqual(result, "ABA");
+
+        result = LSystem.Compute(axiom, rules, 3);
+        Assert.AreEqual(result, "ABAAB");
+
+        result = LSystem.Compute(axiom, rules, 4);
+        Assert.AreEqual(result, "ABAABABA");
+
+        result = LSystem.Compute(axiom, rules, 5);
+        Assert.AreEqual(result, "ABAABABAABAAB");
+
+        result = LSystem.Compute(axiom, rules, 6);
+        Assert.AreEqual(result, "ABAABABAABAABABAABABA");
+
+        result = LSystem.Compute(axiom, rules, 7);
+        Assert.AreEqual(result, "ABAABABAABAABABAABABAABAABABAABAAB");
+    }
+
+    /// <summary>
+    /// Taken from https://en.wikipedia.org/wiki/L-system
+    /// 
+    /// Variables: 0, 1
+    /// Constants: [, ]
+    /// Axiom: 0
+    /// Rules: (1 => 11), (0 => 1[0]0)
+    /// </summary>
+    [Test]
+    public void ItShouldKeepConstants() {
+        string axiom = "0";
+
+        IDictionary<string, string> rules = new Dictionary<string, string>() {
+            { "1", "11" },
+            { "0", "1[0]0" }
+        };
+
+        string result = LSystem.Compute(axiom, rules, 0);
+        Assert.AreEqual(result, "0");
+
+        result = LSystem.Compute(axiom, rules, 1);
+        Assert.AreEqual(result, "1[0]0");
+
+        result = LSystem.Compute(axiom, rules, 2);
+        Assert.AreEqual(result, "11[1[0]0]1[0]0");
+
+        result = LSystem.Compute(axiom, rules, 3);
+        Assert.AreEqual(result, "1111[11[1[0]0]1[0]0]11[1[0]0]1[0]0");
     }
 }
